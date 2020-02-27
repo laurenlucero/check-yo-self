3de08a3d01b62c86b437bd0a98cdfb9ae05f93b2
“Lauren <lucerolaurenm@gmail.com>
2020-02-27 11:19:42 -0700

    Add html boilerplate and JS architectur, CSS reset and JS architecture

---
+++ task.js
@@ -0,0 +1,7 @@
+console.log('Task test');
+
+Task{
+  constructor() {
+    
+  }
+}
---
+++ todo-list.js
@@ -0,0 +1,19 @@
+console.log('Todo-list test');
+
+class ToDoList {
+  constructor() {
+
+  }
+  savetoStorage() {
+    // save to local storage
+  }
+  deleteFromStorage() {
+    // remove from local storage
+  }
+  updateToDo() {
+    // should update the todo's title and urgency
+  }
+  updateTask() {
+    // should update a task's content and if it has been completed
+  }
+}
